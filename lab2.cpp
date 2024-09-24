9f66fed97d19127c3c2dfd4f7087edf0746e9e08
Artem Timofeenko <timofeenkoartem78@gmail.com>
2024-09-24 20:35:03 +0300

    Update lab2.cpp

---
+++ lab2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+long double factorial(long double chislo){
+    long double result =1;
+    for (int i=chislo;i>0;i-=2){
+        result*=i;
+    }
+    return result;
+}
+
+double stepen(double base, int power)
+{
+	double result = 1;
+	int k = 0;
+	while (k < power)
+	{
+		result *= base;
+		k++;
+	}
+	return result;
+}
+long double formula(long double chislo){
+    long double chisl=0,znam=0;
+    for (int k=0;k<=10;k++){
+        chisl+=stepen(chislo,2*k+1)/factorial(2*k+1);
+    }
+    for (int k=0;k<=10;k++){
+        znam+=stepen(chislo,2*k)/factorial(2*k);
+    }
+    return chisl/znam;
+}
+
+int main(){
+    long double y,result,chisl,znam;
+    std::cout<<"Введите y:";
+    std::cin>>y;
+    chisl=7*formula(0.25)+2*formula(1+y);
+    znam=6-formula(stepen(y,2)-1);
+    result=chisl/znam;
+    std::cout<<"Результат:"<<result;
+    return 0;
+}
